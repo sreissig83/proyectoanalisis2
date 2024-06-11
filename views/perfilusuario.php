d3eb78d90b9b552bc6f4419df52cfa1860b9d7b7
​Sebastian Reissig <sreissig83@gmail.com>
2024-06-11 15:48:57 -0300

    perfilusuario, eliminacion registro y configuracio

--- views/perfilusuario.php
+++ views/perfilusuario.php
@@ -7,6 +7,9 @@
     $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
 
 ?> 
+<?php
+foreach($data as $dat) {
+?> 
 <div class="container">
     <div class="row align.items-center text-center">
         <div class="col-md-4">
@@ -25,23 +28,45 @@
     <div class="row align.items-center text-center">
             <div class="col-md-4">
                 <h4>Nombre:</h4>
-                <span><?php echo $_SESSION["s_usuario"];?></span>
+                <span><?php echo $dat['usuario_nombres']?></span>
             </div>
             <div class="col-md-4">
                 <h4>Apellido:</h4>
-                <span><?php echo $_SESSION["s_idRol"];?></span>
+                <span><?php echo $dat['usuario_apellidos']?></span>
             </div>       
             <div class="col-md-4">
                 <h4>DNI:</h4>
-                <span><?php echo $_SESSION["s_rol_descripcion"];?></span>
+                <span><?php echo $dat['usuario_dni']?></span>
             </div>
             <div class="col-md-4">
                 <h4>Correo:</h4>
-                <span><?php echo $_SESSION["s_rol_descripcion"];?></span>
+                <span><?php echo $dat['usuario_correo']?></span>
             </div>
     </div>
 
 </div>
-
+<?php
+                                }
+?>  
 
 <?php require_once "parte_inferior.php"?>
+
+<?php
+                            foreach($data as $dat) {
+                            ?>    
+                            <tr>
+                                <td class="text-center"><?php echo $dat['usuario_id']?></td>
+                                <td class="text-center"><?php echo $dat['usuario_uname']?></td>
+                                <td class="text-center" ><?php echo $dat['usuario_passk']?></td>
+                                <td class="text-center"><?php echo $dat['usuario_nombres']?></td>
+                                <td class="text-center"><?php echo $dat['usuario_apellidos']?></td>
+                                <td class="text-center"><?php echo $dat['usuario_dni']?></td>
+                                <td class="text-center"><?php echo $dat['usuario_correo']?></td>
+                                <td class="text-center"><?php echo $dat['roles']?></td>
+                                <td>
+                                    
+                                </td>
+                            </tr>
+                            <?php
+                                }
+                            ?>    
